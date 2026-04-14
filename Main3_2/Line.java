d11f34f14229d26cc61ebf171b5ff430d12d0805
wdwq <wdwq6231@gmail.com>
2026-04-14 11:38:42 +0000

    例28

---
+++ Main3_2/Line.java
@@ -0,0 +1,17 @@
+package Main3_2;
+
+public class Line {
+    InputScore one;
+    DelScore two;
+    ComputerAver three;
+
+    Line(){
+        three = new ComputerAver();
+        two = new DelScore(three);
+        one = new InputScore(two);
+    }
+
+    public void givePersonScore(){
+        one.inputScore();
+    }
+}
---
+++ Main3_2/SingGame.java
@@ -0,0 +1,8 @@
+package Main3_2;
+
+public class SingGame {
+    public static void main(String args[]){
+        Line line = new Line();
+        line.givePersonScore();
+    }
+}
