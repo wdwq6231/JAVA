ac99dd6bedebddf4fda858c09911c1eebb6f6bbe
wdwq <wdwq6231@gmail.com>
2026-05-06 05:18:41 +0000

    编程题7

---
+++ Main6_2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main6_2 {
+    public static void main(String args[]){
+        Scanner reader = new Scanner(System.in);
+        double sum = 0;
+        int m = 0;
+
+        while(reader.hasNextDouble()){
+            double x = reader.nextDouble();
+            
+            assert x >= 0 && x <= 100 : "这是一个非法的成绩数据";
+            
+            m = m + 1;
+            sum = sum + x;
+        }
+
+        System.out.printf("%d个数的和为%f\n",m,sum);
+        System.out.printf("%d个数的平均值是%f\n",m,sum/m);
+    }
+}
